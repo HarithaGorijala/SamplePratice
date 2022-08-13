cee1e40e651be3c5a891c7e69f0edc00a0b5f0ca
HarithaGorijala <harithagvl@gmail.com>
2022-08-13 15:05:06 +0530

    initial

---
+++ JUnitExample/src/main/java/com/antra/pack/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.antra.pack;
+
+public class HelloWorld {
+   public float div(int a,int b) {
+	  
+	   return a/b;
+	}
+
+}
---
+++ JUnitExample/src/test/java/com/antra/pack/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.antra.pack;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHelloWorld {
+	@Test
+	public void testadd() {
+		HelloWorld h=new HelloWorld();
+		System.out.println(h.div(10, -20));
+		
+	}
+
+}
